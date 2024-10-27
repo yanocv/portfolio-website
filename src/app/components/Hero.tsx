984170afcb4c47b7d37d12d83b028ef4082c347e
Yano <51319482+yanocv@users.noreply.github.com>
2024-10-27 11:03:51 +0900

    Fix: Image src

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -22,7 +22,7 @@ const Hero: React.FC = (): JSX.Element => {
 
           <div className="col-md-6 text-right">
             <Image
-              src="/portfolio-website/public/assets/img/avatar.png"
+              src="assets/img/avatar.png"
               alt="my photo"
               width={500}
               height={600}
